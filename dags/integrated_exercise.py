59f18150dad564bcf41a3ae790e6ac02c09ddcfc
Yves De Boeck <yvesdeboeck6@gmail.com>
2023-11-27 21:03:42 +0000

    adjust tested dag

--- dags/integrated_exercise.py
+++ dags/integrated_exercise.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from airflow.providers.amazon.aws.operators.batch import BatchOperator
 import datetime as dt
 
-dag = DAG(
+ingesting_dag = DAG(
     dag_id="ingesting_pipeline",
     description="Extract data from the geo.irceline.be api and loads into s3",
     default_args={"owner": "Yves De Boeck"},
@@ -10,16 +10,20 @@ dag = DAG(
     start_date=dt.datetime(2023, 11, 20),
 )
 
-ingest = BatchOperator(
-    job_name='yves-ingest',
-    job_definition='yves-integrated-exercise',
-    job_queue='integrated-exercise-job-queue',
-    container_overrides={"command": [
-      "python",
-      "./ingest.py",
-      "-d",
-      "2023-11-23",
-      "-e",
-      "dev"
-    ]},
-)
+
+with ingesting_dag:
+    ingest = BatchOperator(
+        task_id="yves-ingest",
+        job_name="yves-ingest",
+        job_definition="yves-integrated-exercise",
+        job_queue="integrated-exercise-job-queue",
+        region_name="eu-west-1",
+        overrides={"command": [
+        "python",
+        "./ingest.py",
+        "-d",
+        "2023-11-23",
+        "-e",
+        "dev"
+        ]},
+    )
