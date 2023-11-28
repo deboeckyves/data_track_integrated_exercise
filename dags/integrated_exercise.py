4aaf3830938a713a3a7d17fa50c51ebf70ae8ea8
Yves De Boeck <yvesdeboeck6@gmail.com>
2023-11-28 22:18:26 +0000

    updates to ingest and transform

--- dags/integrated_exercise.py
+++ dags/integrated_exercise.py
@@ -2,20 +2,20 @@ from airflow import DAG
 from airflow.providers.amazon.aws.operators.batch import BatchOperator
 import datetime as dt
 
-ingesting_dag = DAG(
-    dag_id="ingesting_pipeline",
+dag = DAG(
+    dag_id="yves-integrated-exercise-pipeline",
     description="Extract data from the geo.irceline.be api and loads into s3",
     default_args={"owner": "Yves De Boeck"},
     schedule_interval="@daily",
-    start_date=dt.datetime(2023, 11, 20),
+    start_date=dt.datetime(2023, 11, 24),
 )
 
 
-with ingesting_dag:
+with dag:
     ingest = BatchOperator(
         task_id="yves-ingest",
         job_name="yves-ingest",
-        job_definition="yves-integrated-exercise",
+        job_definition="yves-integrated-excercise-ingest",
         job_queue="integrated-exercise-job-queue",
         region_name="eu-west-1",
         overrides={"command": [
@@ -27,3 +27,21 @@ with ingesting_dag:
         "dev"
         ]},
     )
+
+    transform = BatchOperator(
+        task_id="yves-transform",
+        job_name="yves-transform",
+        job_definition="yves-integrated-exercise-transform",
+        job_queue="integrated-exercise-job-queue",
+        region_name="eu-west-1",
+        overrides={"command": [
+        "python",
+        "./transform.py",
+        "-d",
+        "{{ ds }}",
+        "-e",
+        "dev"
+        ]},
+    )
+
+ingest >> transform
--- src/integratedexercise/ingest.py
+++ src/integratedexercise/ingest.py
@@ -31,17 +31,39 @@ def process_raw_data_for_station_v2(station: dict, date: str):
     body = {"timespan": f"PT24h/{date}TZ", "timeseries": list(timeseries_keys)}
     timeseries = requests.post(f'https://geo.irceline.be/sos/api/v1/timeseries/getData', data=json.dumps(body), headers=headers )
 
-
+    station_records = []
+    print(station)
+    print("------------------------------------------")
+    print(timeseries_keys)
+    print('-------------------------------------------')
+    print(timeseries.json())
     for timeserie_key in timeseries_keys:
         station_response['properties']['timeseries'][timeserie_key]['values'] = timeseries.json()[timeserie_key]['values']
         for value in timeseries.json()[timeserie_key]['values']:
-            value['station_id'] = station_id
-            value['station_label'] = station_id = station['properties']['label']
-            value['timeseries_id'] = timeserie_key
-            value['timeseries_id'] = timeserie_key 
+            result_day_station_metric = value
+            result_day_station_metric['station_id'] = station_id
+            result_day_station_metric['station_label'] = station['properties']['label']
+            result_day_station_metric['station_geometry_coordinates_x'] = station['geometry']['coordinates'][0]
+            result_day_station_metric['station_geometry_coordinates_y'] = station['geometry']['coordinates'][1]
+            result_day_station_metric['station_geometry_coordinates_z'] = station['geometry']['coordinates'][2]
+            result_day_station_metric['station_geometry_type'] = station['geometry']['type']
+            result_day_station_metric['station_type'] = station['type']
+            result_day_station_metric['timeseries_id'] = timeserie_key
+            result_day_station_metric['service_id'] = station_response['properties']['timeseries'][timeserie_key]['service']['id']
+            result_day_station_metric['service_label'] = station_response['properties']['timeseries'][timeserie_key]['service']['label']
+            result_day_station_metric['offering_id'] = station_response['properties']['timeseries'][timeserie_key]['offering']['id']
+            result_day_station_metric['offering_label'] = station_response['properties']['timeseries'][timeserie_key]['offering']['label']
+            result_day_station_metric['feature_id'] = station_response['properties']['timeseries'][timeserie_key]['feature']['id']
+            result_day_station_metric['feature_label'] = station_response['properties']['timeseries'][timeserie_key]['feature']['label']
+            result_day_station_metric['procedure_id'] = station_response['properties']['timeseries'][timeserie_key]['procedure']['id']
+            result_day_station_metric['procedure_label'] = station_response['properties']['timeseries'][timeserie_key]['procedure']['label']
+            result_day_station_metric['phenomenon_id'] = station_response['properties']['timeseries'][timeserie_key]['phenomenon']['id']
+            result_day_station_metric['phenomenon_label'] = station_response['properties']['timeseries'][timeserie_key]['phenomenon']['label']
+            result_day_station_metric['category_id'] = station_response['properties']['timeseries'][timeserie_key]['category']['id']
+            result_day_station_metric['category_label'] = station_response['properties']['timeseries'][timeserie_key]['category']['label']
+            station_records.append(result_day_station_metric)
 
-    station['timeseries'] = station_response['properties']['timeseries']
-    return station
+    return station_records
 
 def write_json_to_s3(s3_bucket: str, key: str, target_json: dict):
     object = s3.Object(s3_bucket, key)
@@ -50,9 +72,11 @@ def write_json_to_s3(s3_bucket: str, key: str, target_json: dict):
 def ingest_data(env, date):
     all_stations_response = requests.get('https://geo.irceline.be/sos/api/v1/stations')
     for station in all_stations_response.json(): 
-        station = process_raw_data_for_station(station, date)
+        # station = process_raw_data_for_station(station, date)
         station_id = station['properties']['id']
-        write_json_to_s3('data-track-integrated-exercise', f'yves-data/{date}/{station_id}.json', station)
+        station_records = process_raw_data_for_station_v2(station, date)
+        #write_json_to_s3('data-track-integrated-exercise', f'yves-data/{date}/{station_id}.json', station)
+        write_json_to_s3('data-track-integrated-exercise', f'yves-data-v2/{date}/{station_id}.json', station_records)
 
 def main():
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
