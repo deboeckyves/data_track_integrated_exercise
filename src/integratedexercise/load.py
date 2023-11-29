351bd5f47bce140a53ffc42f180bc4c5e6e18910
Yves De Boeck <yvesdeboeck6@gmail.com>
2023-11-29 15:46:14 +0000

    working load

--- src/integratedexercise/load.py
+++ src/integratedexercise/load.py
@@ -4,17 +4,26 @@ import logging
 import sys
 import boto3
 import pyspark.sql.functions as F
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SQLContext
+from pyspark.sql.types import *
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import Row, SparkSession
 
 from pyspark.sql import SparkSession
 
+
 s3 = boto3.resource('s3')
+
+#scala version = 2.12.18
+#spark version = 3.5.0
 
 spark = SparkSession.builder.config(
     "spark.jars.packages",
     ",".join(
         [
             "org.apache.hadoop:hadoop-aws:3.3.1",
-            "net.snowflake:spark-snowflake_2.13:2.13.0-spark_3.4",
+            "net.snowflake:spark-snowflake_2.12:2.5.4-spark_2.4",
             "net.snowflake:snowflake-jdbc:3.14.3"
         ]
     ),
@@ -24,7 +33,6 @@ spark = SparkSession.builder.config(
 ).getOrCreate()
 
 from botocore.exceptions import ClientError
-
 
 def get_secret(secret_name: str):
 
@@ -78,35 +86,62 @@ def main():
 
     s3_bucket = s3.Bucket(bucket)
 
-    # files = set()
-    # files_in_s3 = [f.key.split(folder + "/")[1].split('/')[0] for f in s3_bucket.objects.filter(Prefix=folder).all()]
-    # files.update(files_in_s3)
-    #
-    # counter = 0
-    # print(files)
-    # df_ppm_all_stations = None
-    # for i, file in enumerate(files):
-    #     if i > 2:
-    #         break
-    #     if i == 0:
-    #         df_ppm_all_stations = spark.read.parquet(
-    #             f"s3a://data-track-integrated-exercise/yves-data-v2/clean/aggregate_station_by_day/{args.date}/{file}/phenomenon_id=5")
-    #     else:
-    #         df = spark.read.parquet(
-    #             f"s3a://data-track-integrated-exercise/yves-data-v2/clean/aggregate_station_by_day/{args.date}/{file}/phenomenon_id=5")
-    #         df_ppm_all_stations.union(df)
-    #
-    #
-    # df_ppm_all_stations.show()
-    # df_ppm_all_stations.show(n=5, truncate=False, vertical=True)
+    files = set()
+    files_in_s3 = [f.key.split(folder + "/")[1].split('/')[0] for f in s3_bucket.objects.filter(Prefix=folder).all()]
+    files.update(files_in_s3)
 
+    counter = 0
+    print(files)
+    df_ppm_all_stations = spark.read.parquet(
+        f"s3a://data-track-integrated-exercise/yves-data-v2/clean/aggregate_station_by_day/{args.date}/1030.json/phenomenon_id=5")
+
+    for file in files:
+        try:
+            df = spark.read.parquet(
+                f"s3a://data-track-integrated-exercise/yves-data-v2/clean/aggregate_station_by_day/{args.date}/{file}/phenomenon_id=5")
+            df_ppm_all_stations = df_ppm_all_stations.union(df)
+        except Exception as err:
+            print(f"Exception fetching ppm data for date {args.date} and file {file}, possibly because this station does not have ppm data: {err}")
+
+    print(df_ppm_all_stations.count())
+    df_ppm_all_stations.show()
+    df_ppm_all_stations.show(n=5, truncate=False, vertical=True)
+
+    print(get_snowflake_creds_from_sm("snowflake/integrated-exercise/yves-login"))
+    sfOptions = {'sfURL': 'yw41113.eu-west-1.snowflakecomputing.com', 'sfPassword': 'ilovedbt', 'sfUser': 'axxes_yves', 'sfDatabase': 'ACADEMY_DBT', 'sfWarehouse': 'COMPUTE_WH', 'sfSchema': 'AXXES_YVES', 'sfRole': 'STUDENT'}
     # read full table
-    df = spark.read.format("snowflake") \
-            .options(**get_snowflake_creds_from_sm("snowflake/integrated-exercise/yves-login")) \
-        .option("dbtable", "AXXES_YVES.customer") \
-        .load()
+    df = spark.read.format("snowflake").options(**sfOptions).option("dbtable", "SNOWFLAKE_CASES_TRANS").load()
 
-    df.show()
+    df_ppm_all_stations.printSchema()
+    table_name = f"PPM_ALL_STATIONS_{args.date.replace('-', '_')}"
+    spark.sparkContext._jvm.net.snowflake.spark.snowflake.Utils.runQuery(sfOptions, f"""CREATE TABLE IF NOT EXISTS {table_name} (
+        category_id VARCHAR,
+        category_label VARCHAR,
+        feature_id VARCHAR,
+        feature_label VARCHAR,
+        offering_id VARCHAR,
+        offering_label VARCHAR,
+        phenomenon_label VARCHAR,
+        procedure_id VARCHAR,
+        procedure_label VARCHAR,
+        service_id VARCHAR,
+        service_label VARCHAR,
+        station_geometry_coordinates_x DOUBLE,
+        station_geometry_coordinates_y DOUBLE,
+        station_geometry_coordinates_z VARCHAR,
+        station_geometry_type VARCHAR,
+        station_id INTEGER,
+        station_label VARCHAR,
+        station_type VARCHAR,
+        timeseries_id VARCHAR,
+        timestamp INTEGER,
+        value DOUBLE,
+        datetime TIMESTAMP,
+        avg_day DOUBLE)""")
+
+    df_ppm_all_stations.write.format("snowflake").options(**sfOptions).option("dbtable", f"{table_name}").mode("overwrite").save()
+
+
 
 if __name__ == "__main__":
     main()
