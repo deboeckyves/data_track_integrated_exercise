0c5db3e8a2dbf5343ce8fabea0b6d5dc8e1f255c
deboeckyves <yves.deboeck6@gmail.com>
2023-11-30 15:42:20 +0100

    fix terraform

---
+++ dags/integrated_exercise-tf.py
@@ -0,0 +1,63 @@
+from airflow import DAG
+from airflow.providers.amazon.aws.operators.batch import BatchOperator
+import datetime as dt
+
+dag = DAG(
+    dag_id="yves-integrated-exercise-pipeline",
+    description="Extract data from the geo.irceline.be api and loads into s3",
+    default_args={"owner": "Yves De Boeck"},
+    schedule_interval="@daily",
+    start_date=dt.datetime(2023, 11, 24),
+)
+
+
+with dag:
+    ingest = BatchOperator(
+        task_id="yves-ingest",
+        job_name="yves-ingest",
+        job_definition="yves-integrated-excercise-ingest",
+        job_queue="integrated-exercise-job-queue",
+        region_name="eu-west-1",
+        overrides={"command": [
+        "python",
+        "./ingest.py",
+        "-d",
+        "{{ ds }}",
+        "-e",
+        "dev"
+        ]},
+    )
+
+    transform = BatchOperator(
+        task_id="yves-transform",
+        job_name="yves-transform",
+        job_definition="yves-integrated-exercise-transform",
+        job_queue="integrated-exercise-job-queue",
+        region_name="eu-west-1",
+        overrides={"command": [
+        "python3",
+        "./transform.py",
+        "-d",
+        "{{ ds }}",
+        "-e",
+        "dev"
+        ]},
+    )
+
+    load = BatchOperator(
+        task_id="yves-load",
+        job_name="yves-load",
+        job_definition="yves-integrated-exercise-load",
+        job_queue="integrated-exercise-job-queue",
+        region_name="eu-west-1",
+        overrides={"command": [
+            "python3",
+            "./load.py",
+            "-d",
+            "{{ ds }}",
+            "-e",
+            "dev"
+        ]},
+    )
+
+ingest >> transform >> load
