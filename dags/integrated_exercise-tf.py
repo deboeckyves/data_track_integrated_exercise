93fa1d6f2960ccccdea2a836db37b533e3de00b4
deboeckyves <deboeckyves6@gmail.com>
2023-12-13 09:53:55 +0100

    adjust dag run for whole year

--- dags/integrated_exercise-tf.py
+++ dags/integrated_exercise-tf.py
@@ -7,7 +7,7 @@ dag = DAG(
     description="Extract data from the geo.irceline.be api and loads into s3",
     default_args={"owner": "Yves De Boeck"},
     schedule_interval="@daily",
-    start_date=dt.datetime(2023, 11, 24),
+    start_date=dt.datetime(2023, 1, 1),
 )
 
 
